a2ba813a1e82b06aafd9ac004c7510abb79bc265
Max <max@maximilian.co.nz>
2024-01-19 23:56:32 +1300

    Highlight thing

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -10,6 +10,8 @@ class Player : GameObject
 
 	public static float Width = 100f;
 
+	public static Vector2 HighlightedTile;
+
 	public override void Start()
 	{
 		// Create a new camera
@@ -28,11 +30,17 @@ class Player : GameObject
 		// TODO: Make it so the camera stops when the player reaches the end of the world, so they can never see out
 		Movement();
 		Camera.Target = Position - new Vector2((Raylib.GetScreenWidth() / 2) - (Width / 2 ), Raylib.GetScreenHeight() / 2);
+
+		Highlight();
 	}
 
 	public override void Render()
 	{
+		// Draw the player
 		Raylib.DrawRectangle((int)Position.X, (int)Position.Y, (int)Width, 100, Color.RED);
+
+		// Draw the highlighted tile
+		Raylib.DrawRectangle((int)HighlightedTile.X * World.TileSize, (int)HighlightedTile.Y * World.TileSize, World.TileSize * World.TileScale, World.TileSize * World.TileScale, new Color(0, 0, 255, 128));
 	}
 
 	public override void CleanUp()
@@ -60,5 +68,19 @@ class Player : GameObject
 
 		// Apply the movement
 		Position = newPosition;
+	}
+
+	// Highlight the current tile that the player is on
+	private void Highlight()
+	{
+		// Get the mouses coordinates, then convert them to world coordinates
+		Vector2 screenCoordinates = Raylib.GetMousePosition();
+		Vector2 worldCoordinates = Raylib.GetScreenToWorld2D(screenCoordinates, Camera);
+
+		// Snap the world coordinates to the grid
+		HighlightedTile = new Vector2(
+			(int)worldCoordinates.X / World.TileSize,
+			(int)worldCoordinates.Y / World.TileSize
+		);
 	}
 }
