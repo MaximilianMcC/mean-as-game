18084f6a5acf1cb6447e08d73eca23ddda7f81ee
MaximilianMcC <max@maximilian.co.nz>
2024-09-10 13:30:24 +1200

    Music and sound support

--- src/Program.cs
+++ src/Program.cs
@@ -14,6 +14,7 @@ class Program
 		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
 		Raylib.InitWindow(800, 600, "mean as game (really cool (epic))");
 		Raylib.SetExitKey(KeyboardKey.Null);
+		Raylib.InitAudioDevice();
 
 		Game.Start();
 		while (!Raylib.WindowShouldClose())
@@ -22,6 +23,7 @@ class Program
 			Game.Render();
 		}
 		Game.CleanUp();
+		Raylib.CloseAudioDevice();
 		Raylib.CloseWindow();
 	}
 }
