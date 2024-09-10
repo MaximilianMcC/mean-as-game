3c7f28b3299492d043a24c563e1a0976d64f60b9
MaximilianMcC <max@maximilian.co.nz>
2024-09-10 12:12:03 +1200

    Super scuffed pause thing

--- src/Game/Game.cs
+++ src/Game/Game.cs
@@ -5,6 +5,7 @@ class Game
 {
 	private static Player player;
 	private static List<GameObject> gameObjects;
+	public static bool Paused = false;
 
 	public static void Start()
 	{
@@ -18,6 +19,14 @@ class Game
 
 	public static void Update()
 	{
+		// Check for if we wanna toggle being paused
+		if (Raylib.IsWindowFocused() == false) Paused = true;
+		if (Raylib.IsKeyPressed(KeyboardKey.Escape)) Paused = !Paused;
+
+		// Check for if we're paused
+		if (Paused == true) return;
+
+		// Update everything
 		foreach (GameObject gameObject in gameObjects)
 		{
 			gameObject.Update();
@@ -29,10 +38,20 @@ class Game
 		Raylib.BeginDrawing();
 		Raylib.ClearBackground(Color.Magenta);
 
+		// Draw everything
 		Map.Render();
 		foreach (GameObject gameObject in gameObjects)
 		{
 			gameObject.Render();
+		}
+
+		// If we're paused then darken the screen
+		// and also say that we're paused
+		if (Paused)
+		{
+			Vector2 screen = new Vector2(Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
+			Raylib.DrawRectangleV(Vector2.Zero, screen, new Color(0, 0, 0, 128));
+			Raylib.DrawTextEx(Ui.TimesNewRoman, $"Paused rn", screen / 2, 35f, (35f / 10f), Color.White);
 		}
 
 		Raylib.EndDrawing();
--- src/Program.cs
+++ src/Program.cs
@@ -13,6 +13,7 @@ class Program
 	{
 		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
 		Raylib.InitWindow(800, 600, "mean as game (really cool (epic))");
+		Raylib.SetExitKey(KeyboardKey.Null);
 
 		Game.Start();
 		while (!Raylib.WindowShouldClose())
