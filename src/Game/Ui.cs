a6618270a4ede5ae6dc396874d41fd9cf513cdde
MaximilianMcC <max@maximilian.co.nz>
2024-09-11 00:04:16 +1200

    Added new font

--- src/Game/Ui.cs
+++ src/Game/Ui.cs
@@ -3,14 +3,17 @@ using Raylib_cs;
 class Ui
 {
 	public static Font TimesNewRoman;
+	public static Font Gabriola;
 
 	public static void Load()
 	{
 		TimesNewRoman = AssetManager.LoadFont("./assets/ui/times.ttf");
+		Gabriola = AssetManager.LoadFont("./assets/ui/gabriola.ttf");
 	}
 
 	public static void CleanUp()
 	{
 		Raylib.UnloadFont(TimesNewRoman);
+		Raylib.UnloadFont(Gabriola);
 	}
 }
